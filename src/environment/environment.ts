78e518836bf04f80fe350303ac02b4d87a38e875
SKY <onthsky@gmail.com>
2023-03-29 15:56:51 +0300

    Angular configuration has added

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBjxBtN9khkAAM8G-p7sGtM_qGowyTJtzY',
+    authDomain: 'fir-student-managment.firebaseapp.com',
+    projectId: 'fir-student-managment',
+    storageBucket: 'fir-student-managment.appspot.com',
+    messagingSenderId: '141019432203',
+    appId: '1:141019432203:web:d081ae36da7db954494e0e',
+  },
+};
